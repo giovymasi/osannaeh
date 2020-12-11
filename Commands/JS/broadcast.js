e9d2071a6e84033729cee6e3c04c1a149307f74e
giovymasi <42315587+giovymasi@users.noreply.github.com>
2020-12-11 13:57:07 +0100

    Add files via upload

---
+++ Commands/JS/broadcast.js
@@ -0,0 +1,70 @@
+﻿function command() {
+    try {
+        var message = "";
+        if (args[0] == "group") {
+            var group = new Group(kArgs[1]);
+            if (group == null) {
+                JSBot.SendTextMessage("Il bot non fa parte del gruppo indicato");
+                return;
+            }
+            args.shift();
+            args.shift();
+            for (var word of args) {
+                message += `${word} `;
+            }
+            try {
+                JSBot.SendTextMessage(group.group_id, message);
+                JSBot.SendTextMessage(Utilities.ShortnameToEmoji(":white_check_mark:Messaggio inviato"));
+            } catch {
+                JSBot.SendTextMessage(Utilities.ShortnameToEmoji(":x:Non sono riuscito a inviare il messaggio"));
+            }
+        }
+        else if (args[0] == "user") {
+            var user = Database.GetUser(args[1].replace("@", ""));
+            args.shift();
+            args.shift();
+            for (var word1 of args) {
+                message += `${word1} `;
+            }
+            try {
+                JSBot.SendTextMessage(user.id, message);
+                JSBot.SendTextMessage(Utilities.ShortnameToEmoji(":white_check_mark:Messaggio inviato"));
+            } catch {
+                JSBot.SendTextMessage(Utilities.ShortnameToEmoji(":x:Non sono riuscito a inviare il messaggio"));
+            }
+        }
+        else if (args[0] == "all") {
+            var thisgroup = new Group(JSBot.MessageArgs.Message.Chat.Title);
+            users = thisgroup.users;
+            args.shift();
+            for (var word2 of args) {
+                message += `${word2} `;
+            }
+            for (var utente of users) {
+                try {
+                    JSBot.SendTextMessage(utente.id, message);
+                } catch { }
+            }
+        }
+        else if (args[0] == "allshow") {
+            var thisgroup1 = new Group(JSBot.MessageArgs.Message.Chat.Title);
+            users1 = thisgroup1.users;
+            args.shift();
+            for (var word3 of args) {
+                message += `${word3} `;
+            }
+            for (var utente1 of users1) {
+                try {
+                    JSBot.SendTextMessage(utente1.id, message);
+                    JSBot.SendTextMessage(Utilities.ShortnameToEmoji(`:white_check_mark:@${utente1.username} ha ricevuto il messaggio`));
+                } catch {
+                    JSBot.SendTextMessage(Utilities.ShortnameToEmoji(`:x:@${utente1.username} non ha ricevuto il messaggio`));
+                }
+            }
+        }
+    }
+    catch (error) {
+        JSBot.SendTextMessage("Error: " + error);
+    }
+}
+command();
