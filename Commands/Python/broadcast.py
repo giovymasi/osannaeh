9b34dc4eb742809226b83a648d5b0237bd8b67b7
giovymasi <42315587+giovymasi@users.noreply.github.com>
2020-12-11 11:50:19 +0100

    Add files via upload

--- Commands/Python/broadcast.py
+++ Commands/Python/broadcast.py
@@ -5,28 +5,28 @@ try:
     if args[0]=="group":
         group = types.Group(args[1])
         if group == None:
-            PyBot.SendTextMessage("Il bot non e' stato inserito in questo gruppo.")
+            PyBot.SendTextMessage("Il bot non e' stato inserito in questo gruppo.".replace("<", "-"))
             sys.exit
         args.remove(args[0])
         args.remove(args[1])
         for word in args:
             message += word + " "
-        PyBot.SendTextMessage(group.group_id, message)
-        PyBot.SendTextMessage(Utilities.ShortnameToEmoji(":white_check_mark") + "Inviato")
+        PyBot.SendTextMessage(group.group_id, message.replace("<", "-"))
+        PyBot.SendTextMessage(Utilities.ShortnameToEmoji(":white_check_mark") + "Inviato".replace("<", "-"))
     elif args[0]=="user":
         user = types.Database.GetUser(args[1].replace("@", ""))
         if user == None:
-            PyBot.SendTextMessage("L'utente non e' registrato.")
+            PyBot.SendTextMessage("L'utente non e' registrato.".replace("<", "-"))
             sys.exit
         args.remove(args[0])
         args.remove(args[1])
         for word1 in args:
             message += word1 + " "
         try:
-            PyBot.SendTextMessage(user.id, message)
-            PyBot.SendTextMessage(Utilities.ShortnameToEmoji(":white_check_mark") + "Inviato")
+            PyBot.SendTextMessage(user.id, message.replace("<", "-"))
+            PyBot.SendTextMessage(Utilities.ShortnameToEmoji(":white_check_mark") + "Inviato".replace("<", "-"))
         except:
-            PyBot.SendTextMessage(Utilities.ShortnameToEmoji(":x:") + "Non sono riuscito a inviare il messaggio")
+            PyBot.SendTextMessage(Utilities.ShortnameToEmoji(":x:") + "Non sono riuscito a inviare il messaggio".replace("<", "-"))
     elif args[0] == "all":
         thisgroup = types.Group(PyBot.MessageArgs.Message.Chat.Title)
         args.remove(args[0])
@@ -34,7 +34,7 @@ try:
             message += word2 + " "
         for utente in thisgroup.users:
             try:
-                PyBot.SendTextMessage(utente.id, message)
+                PyBot.SendTextMessage(utente.id, message.replace("<", "-"))
             except:
                 pass
     elif args[0] == "allshow":
@@ -44,9 +44,9 @@ try:
             message += word3 + " "
         for utente1 in thisgroup1.users:
             try:
-                PyBot.SendTextMessage(utente1.id, message)
-                PyBot.SendTextMessage(Utilities.ShortnameToEmoji(":white_check_mark:") + "@" + utente1.username + "ha ricevuto il messaggio")
+                PyBot.SendTextMessage(utente1.id, message.replace("<", "-"))
+                PyBot.SendTextMessage(Utilities.ShortnameToEmoji(":white_check_mark:") + "@" + utente1.username + "ha ricevuto il messaggio".replace("<", "-"))
             except:
-                PyBot.SendTextMessage(Utilities.ShortnameToEmoji(":x:") + "@" + utente1.username + "non ha ricevuto il messaggio")
+                PyBot.SendTextMessage(Utilities.ShortnameToEmoji(":x:") + "@" + utente1.username + "non ha ricevuto il messaggio".replace("<", "-"))
 except Exception:
-    PyBot.SendTextMessage(str(Exception))
+    PyBot.SendTextMessage(str(Exception).replace("<", "-"))
