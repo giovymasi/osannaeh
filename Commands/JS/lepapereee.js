f7515f3cacb16f532026627c4e02dd8a6344e70c
giovymasi <gionmasi.masi@gmail.com>
2020-12-22 17:43:42 +0100

    .

---
+++ Commands/JS/lepapereee.js
@@ -0,0 +1,7 @@
+function command() {
+    try {
+        JSBot.SendAudioMessage("https://raw.githubusercontent.com/giovymasi/StockRussoBOT/master/Media/LE%20PAPEREEE.ogg");
+    } catch (error) {
+        JSBot.SendTextMessage(error);
+    }
+}
